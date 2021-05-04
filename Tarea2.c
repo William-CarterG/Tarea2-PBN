62def16ba065c399c836a05c85df62c9dae6eb80
William-CarterG <wgcarter@miuandes.cl>
2021-05-03 20:59:26 -0400

    Ready to start optimizing

--- Tarea2.c
+++ Tarea2.c
@@ -9,31 +9,6 @@ double memoria() {
 	getrusage(RUSAGE_SELF, &r) ;
 	return (double) r.ru_maxrss ;
 }
-/*
-int line_size( FILE *const file_line ){
-    int ch; int c = 0;
-    for( ;; ) {
-        ch = fgetc(file_line);
-
-        if(ch == EOF || ch == '\n')
-            break;
-        ++c  ;
-        printf("%d",c);
-        sleep(2) ;
-    }
-    return c;
-}
-
-int number_lines (FILE *const file_line ){
-	char character; int count = 0 ;
-	 for (character = getc(file_line); character != EOF; character = getc(file_line)){
-
-        if (character == '\n') {
-            count = count + 1;
-    	}
-    }
-    return count ;
-}*/
 
 int main(int argc, char ** argv) {
 	double t = clock() ;
@@ -44,14 +19,11 @@ int main(int argc, char ** argv) {
 	    	perror("La popularidad que buscas no es válida.\n");
 	    	exit(EXIT_FAILURE);
 		}
-	    FILE *fp = fopen("genres.txt", "r");
-	    //FILE *genres1 = fopen("genres.txt", "r") ;
+	    FILE *genres = fopen("genres.txt", "r");
 		int n = 13000000 ; 
-		//number_lines(genres1) Conseguía el número de líneas del archivo de texto, 
-		//pero era más lento y al final es innecesario
 	    
 		//Esto no debería pasar nunca, pero aún así supongo que es buena práctica :)
-	    if (fp == 0){
+	    if (genres == 0){
 	        fprintf(stderr, "Failed to open file 'genres.txt' for reading\n");
 	        return 1;
 	    }
@@ -60,7 +32,7 @@ int main(int argc, char ** argv) {
 	    char *buffer1 ; buffer1 = malloc(4000*sizeof(char)) ;
 	    char *line ; int counter = 0 ;
 
-	    for (int i = 0; i < n && fgets(buffer1, 4000, fp) != 0; i++){
+	    for (int i = 0; i < n && fgets(buffer1, 4000, genres) != 0; i++){
 
 	        buffer1[strcspn(buffer1, "\n")] = '\0';
 	        line = strtok(strdup(buffer1), ";") ;
@@ -73,6 +45,7 @@ int main(int argc, char ** argv) {
 
 	        }
 	    }
+	    //Si no agregue nada en la primera posición, es porque el archivo de texto no conoce el género escrito
 	    if (names[0] == NULL){
 	    	t = (clock()-t)/CLOCKS_PER_SEC ; printf("tiempo : %lf\n",t) ;
 	    	printf("memoria : %lf KB\n", memoria()) ;
@@ -82,6 +55,10 @@ int main(int argc, char ** argv) {
 	    names = realloc(names, counter*sizeof(char*)) ; id = realloc(id, counter*sizeof(char*)) ;
 
 	    FILE *songs = fopen("songs.txt", "r") ;
+	    if (songs == 0){
+	        fprintf(stderr, "Failed to open file 'songs.txt' for reading\n");
+	        return 1;
+	    }
 	    char *buffer ; buffer = malloc(4000*sizeof(char)) ;
 	    int p ;
 	    char **popularity ; popularity = malloc(counter*sizeof(char*)) ;
@@ -125,13 +102,20 @@ int main(int argc, char ** argv) {
  		free(popularity) ;
  		free(names); free(id) ;
 	 	free(buffer1) ; free(buffer) ;
-	    fclose(fp); fclose(songs); //fclose(genres1);
+	    fclose(genres); fclose(songs); //fclose(genres1);
 	    t = (clock()-t)/CLOCKS_PER_SEC ; printf("tiempo : %lf\n",t) ;
 	    printf("memoria : %lf KB\n", memoria()) ;
 	    return 0;
 	}
 
 	if (strcmp(argv[1],"-p")==0){
+		if (strcmp(argv[2], "Major") != 0 && strcmp(argv[2], "Minor")){
+			t = (clock()-t)/CLOCKS_PER_SEC ; printf("tiempo : %lf\n",t) ;
+	    	printf("memoria : %lf KB\n", memoria()) ;
+	    	perror("Las canciones sólo son de modo 'Major' o 'Minor'.\n");
+	    	exit(EXIT_FAILURE); 
+		}
+
 	    FILE *songs = fopen("songs.txt", "r") ;
 		int n = 13000000 ; // number_lines(genres1);
 	    if (songs == 0){
@@ -154,14 +138,20 @@ int main(int argc, char ** argv) {
 	        	counter += 1 ;
 	        }
         }
-        FILE *fp = fopen("genres.txt", "r");
+        id = realloc(id, counter*sizeof(char*)) ;
+
+        FILE *genres = fopen("genres.txt", "r");
+        if (genres == 0){
+	        fprintf(stderr, "Failed to open file 'genres.txt' for reading\n");
+	        return 1;
+	    }
 
         char **g ; g = malloc(n*sizeof(char*)) ; 
         int *gen ; gen = malloc(n*sizeof(int)) ; char *one_genre ; int amount_genres;
         char *buffer ; buffer = malloc(4000*sizeof(char)) ;
 	    char *l ; int c = 0 ;
 	    
-	    for (int i = 0 ; i<n && fgets(buffer, 4000, fp); i++){
+	    for (int i = 0 ; i<n && fgets(buffer, 4000, genres); i++){
         	buffer[strcspn(buffer, "\n")] = '\0';
 	        l = strtok(strdup(buffer), ";") ;
         	if (i == 0){
@@ -171,17 +161,7 @@ int main(int argc, char ** argv) {
         	else{
  				int flag = 0 ;
 	        	for (int j = 0; j<amount_genres; j++){
-	        		//printf("%d. %s es g[i]\n",i, g[j]) ;
 	        		if (strcmp(g[j], l) == 0){
-	        			l = strtok(NULL, ";") ; //This one is the id ;
-	        			/*
-	        			for (int z = 0; z<counter; z++){
-	        				if (strcmp(l, id[z]) == 0){ 
-	        					gen[j] += 1 ;
-	        					//id[z] = NULL ;
-	        					break ;
-        					}
-    					}*/
 	        			flag = 1 ;
 	        			break ;
 	        		}
@@ -190,13 +170,17 @@ int main(int argc, char ** argv) {
 	        		one_genre = l ; amount_genres += 1; 
 	        		g = realloc(g, amount_genres*sizeof(char*)); g[amount_genres-1] = one_genre ;
 	        		gen = realloc(gen, amount_genres*sizeof(int)) ; gen[amount_genres-1] = 0 ;
-	        		l = strtok(NULL, ";") ;
 	        	}
-
+	        	l = strtok(NULL, ";") ;
 	        	l = strtok(NULL, "\n") ;
 	        }
         }
+
         FILE *genres1 = fopen("genres.txt", "r") ;
+        if (genres1 == 0){
+	        fprintf(stderr, "Failed to open file 'genres.txt' for reading\n");
+	        return 1;
+	    }
         char *buffer2 ; buffer2 = malloc(4000*sizeof(char)) ;
         
 		for(int z=0; z<counter; z++){
@@ -219,16 +203,15 @@ int main(int argc, char ** argv) {
 			}
 		}
 	   		   
-        /*
-        FILE *genres = fopen("genres.txt", "r") ;
-        for (int i = 0 ; i<counter ; i++){
-
-        }*/
         for (int i = 0; i<amount_genres; i++){
         	printf("%s: %d\n", g[i], gen[i]) ;
         }
-        free(g) ; free(gen) ; free(id) ; free(buffer) ; free(buffer1) ;
-        fclose(songs) ; fclose(fp) ;
+
+        free(id) ;
+        free(g) ; free(gen) ;  
+        free(buffer) ; free(buffer1) ; free(buffer2) ;
+        fclose(songs) ; fclose(genres) ; fclose(genres1) ;
+
 	    t = (clock()-t)/CLOCKS_PER_SEC ; printf("tiempo : %lf\n",t) ;
 	    printf("memoria : %lf KB\n", memoria()) ;
 		return 0 ;
